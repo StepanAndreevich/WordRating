ef55d99822b10579ee4c2c613d74bc4ae0421b5b
StepanAndreevich <StepaNKorzoFF@yandex.ru>
2024-09-01 23:21:38 +0300

    1. При запуску обработки файла скопка Старт становится не активной
    2. добавлены комментарии

--- mainwindow.h
+++ mainwindow.h
@@ -5,6 +5,9 @@
 #include <QVariantList>
 #include "filereader.h"
 
+/*!
+ * \brief The MainWindow class класс взаимодействия с графическим интерфейсом
+ */
 class MainWindow : public QObject
 {
     Q_OBJECT
@@ -12,19 +15,59 @@ public:
     explicit MainWindow(QObject *parent = nullptr);
     ~MainWindow();
 
+    /*!
+     * \brief setFilePath Задать путь к файлу
+     * \param filePath
+     */
     Q_INVOKABLE void setFilePath(QString filePath);
+    /*!
+     * \brief start запуск обработки файла
+     */
     Q_INVOKABLE void start();
+    /*!
+     * \brief stop остановка обработки файла
+     */
     Q_INVOKABLE void stop();
+    /*!
+     * \brief fixFilePath очистить строку от лишних символов
+     * \param filePath путь к выбранному файлу
+     * \return
+     */
     Q_INVOKABLE QString fixFilePath(QString filePath);
 
 signals:
+    /*!
+     * \brief fileError
+     * \param errorText текст ошибки
+     */
     void fileError(QString errorText);
-    void outTextReady(QString outText);
+    /*!
+     * \brief processinfFile начало обработки файла
+     * \param filePath путь к файлу
+     */
     void processinfFile(QString filePath);
+    /*!
+     * \brief resultReady подготовленные данные отправляемые в qml
+     * \param wordList список слов
+     * \param entriesList количество вхождений
+     */
     void resultReady(QStringList wordList, QVariantList entriesList);
+    /*!
+     * \brief changeProgress сигнал изменения прогресс бара
+     * \param value процент заполнения прогресс бара
+     */
     void changeProgress(double value);
+    /*!
+     * \brief finished сигнал о завершении обработки файла
+     */
+    void finished();
 
 private:
+    /*!
+     * \brief checkFilePath проверка файла
+     * \param filePath путь к файлу
+     * \return true - с файлом все хорошо / false - с файлом проблемы
+     */
     bool checkFilePath(QString& filePath);
     QString m_filePath = "";
     QThread* m_thread = nullptr;
